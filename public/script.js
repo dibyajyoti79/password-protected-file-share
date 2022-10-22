febadcbfbd4fa663043e6700b4d3e2c129e4a6b5
Dibyajyoti Mahuri <dibyajyoti274@gmail.com>
2022-10-22 23:04:44 +0530

    adding css file

--- public/script.js
+++ public/script.js
@@ -9,12 +9,3 @@ btn.addEventListener('click', function handleClick() {
     console.log("Copied the text: " + link);
 });
 
-window.addEventListener('resize', function () {
-    var mobileWidth = (window.innerWidth > 0) ?
-        window.innerWidth :
-        screen.width;
-    var viewport = (mobileWidth > 360) ?
-        'width=device-width, initial-scale=1.0' :
-        'width=1200';
-    $("meta[name=viewport]").attr('content', viewport);
-});
